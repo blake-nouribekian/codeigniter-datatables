b9a696b8992ae4497fe4f7a546be9d9ebb645b1b
Blake Nouribekian <bnouribekian@yahoo.com>
2012-11-09 17:10:13 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,16 +3,16 @@ $(document).ready(function()
 	$('#data').dataTable({
 		"sScrollY": "400px",
 		"bProcessing": true,
-        "bServerSide": true,
-        "sServerMethod": "GET",
-        "sAjaxSource": "data/getTable",
-        "iDisplayLength": 10,
-        "aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-        "aaSorting": [[0, 'asc']],
-        "aoColumns": [
+	        "bServerSide": true,
+	        "sServerMethod": "GET",
+	        "sAjaxSource": "data/getTable",
+	        "iDisplayLength": 10,
+	        "aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+	        "aaSorting": [[0, 'asc']],
+	        "aoColumns": [
 			{ "bVisible": true, "bSearchable": true, "bSortable": true },
 			{ "bVisible": true, "bSearchable": true, "bSortable": true },
 			{ "bVisible": true, "bSearchable": true, "bSortable": true }
-        ]
+	        ]
 	}).fnSetFilteringDelay(700);
 });
