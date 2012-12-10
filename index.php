a4b351c76c14847cb1413404a5ce704fc9554b7e
Blake Nouribekian <bnouribekian@yahoo.com>
2012-12-10 11:34:26 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title></title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>application/css/jquery.dataTables.css"></link>
+
+    <script type="text/javascript" src="<?php echo base_url(); ?>application/js/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>application/js/javascript.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>application/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>application/js/jquery.dataTables.delay.min.js"></script>
+</head>
+<body>
+
+<table cellpadding="0" cellspacing="0" border="0" id="data" width="100%">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+    <tfoot></tfoot>
+</table>
+
+</body>
+</html>
